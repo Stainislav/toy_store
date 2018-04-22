a7a0016fae438d7f293167d9b480b3f905e4040a
Stanislav <swatogor@mail.ru>
2018-04-22 15:12:12 +0200

    I've added admin to products.

---
+++ products/admin.py
@@ -0,0 +1,41 @@
+# Password 'pavel123'
+
+from django.contrib import admin
+from django.db import models
+from products.models import Category
+
+
+# Категория.
+class CategoryAdmin(Category):
+    list_display = ['id', 'name']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
+
+#    class Meta:
+ #       model = Category
+
+'''
+# Товар.
+class ProductAdmin(models.Model):
+    list_display = ['id', 'name', 'description']
+    list_filter = ['id', 'name', 'description']
+    search_fields = ['id', 'name', 'description']
+
+    class Meta:
+        model = Product
+
+
+# Фотографии товаров.
+class ProductImageAdmin(models.Model):
+    list_display = ['id', 'name', 'description']
+    list_filter = ['id', 'name', 'description']
+    search_fields = ['id', 'name', 'description']
+
+    class Meta:
+        model = ProductImageAdmin
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
+
+'''
+admin.site.register(Category)
+
