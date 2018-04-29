22584dc479b1b9058db3415609cd712eeafae044
Stanislav <swatogor@mail.ru>
2018-04-29 17:07:37 +0200

    I've added urls.py to gitignore

--- mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^', include('products.urls')),
-    #url(r'^', include('orders.urls')),
-    url(r'^admin/', admin.site.urls),
-]
-
