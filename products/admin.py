2344693db3446f288de444190d16e78972d2c9f5
Stanislav <swatogor@mail.ru>
2018-04-30 20:34:30 +0200

    I've added ProductImage model to admin.py

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 from django.db import models
-from products.models import Category, Product
+from products.models import Category, Product, ProductImage
 
 
 # Категория.
@@ -24,18 +24,16 @@ class ProductAdmin(admin.ModelAdmin):
     class Meta:
         model = Product
 
-'''
+
 # Фотографии товаров.
-class ProductImageAdmin(models.Model):
-    list_display = ['id', 'name', 'description']
-    list_filter = ['id', 'name', 'description']
-    search_fields = ['id', 'name', 'description']
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
 
     class Meta:
-        model = ProductImageAdmin
-admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductImage, ProductImageAdmin)
+        model = ProductImage
 
-'''
+admin.site.register(ProductImage, ProductImageAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
