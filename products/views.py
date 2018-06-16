2cb5cfacc9d22d94c360dd4eff3e1bbf5cf976c3
Stainislav <swatogor@mail.ru>
2018-06-16 15:31:44 +0300

    Deleted redundant code

--- products/views.py
+++ products/views.py
@@ -3,25 +3,19 @@ from products.models import Product, ProductImage, Category
 
 
 def home(request):
-    products_images = ProductImage.objects.filter(is_active=True)
-
-    parent_names = Category.objects.filter(parent__name=None)
-    stuffed_toys = Category.objects.filter(parent__name="Мягкие игрушки")
 
     # Categories to show.
-    boy_toys = Category.objects.filter(parent__name="Игрушки для мальчиков")
-    girl_toys = Category.objects.filter(parent__name="Игрушки для девочек")
-    new_toys = Category.objects.filter(name="Новинки")
-    special_for_you_toys = Category.objects.filter(name="Специально для вас")
+    boy_toys       = Category.objects.filter(parent__name="Игрушки для мальчиков")
+    stuffed_toys   = Category.objects.filter(parent__name="Мягкие игрушки")
+    girl_toys      = Category.objects.filter(parent__name="Игрушки для девочек")
+    new_toys       = Category.objects.filter(name="Новинки")
+    children_books = Category.objects.filter(name="Детские книги")
+    hellium_balls  = Category.objects.filter(name="Гелиевые шары")
 
     # Images to show.
     stuffed_toys_images = ProductImage.objects.filter(product__category__parent__name="Мягкие игрушки")
-    boy_toys_images = ProductImage.objects.filter(product__category__parent__name="Игрушки для мальчиков")
-    toys_for_girl_images = products_images.filter(product__category__parent__name="Игрушки для девочек")
-    girl_toys_images = toys_for_girl_images.filter(product__new=False)
-
-    girl_new_toys_images = toys_for_girl_images.filter(product__new=True)
-    special_for_you_toys_images = products_images.filter(product__category__name="Специально для вас")
+    boy_toys_images     = ProductImage.objects.filter(product__category__parent__name="Игрушки для мальчиков")
+    girl_toys_images    = ProductImage.objects.filter(product__category__parent__name="Игрушки для девочек")
 
     return render(request, "home.html", locals())
 
