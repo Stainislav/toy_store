693b6b2b942b6d31715263ff3b79e465c450aa67
Stainislav <swatogor@mail.ru>
2018-09-23 20:23:50 +0300

    Added info pages

--- info/views.py
+++ info/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from info.models import InfoPage
+from products.models import Category
 
 
 def about(request):
+    
+    # Categories to show.
+    boy_toys       = Category.objects.filter(parent__name="Игрушки для мальчиков")
+    stuffed_toys   = Category.objects.filter(parent__name="Мягкие игрушки")
+    girl_toys      = Category.objects.filter(parent__name="Игрушки для девочек")
+    new_toys       = Category.objects.filter(name="Новинки")
+    children_books = Category.objects.filter(name="Детские книги")
+    hellium_balls  = Category.objects.filter(name="Гелиевые шары")
 
     text = InfoPage.objects.filter(name="О нас")
 
@@ -11,12 +20,27 @@ def about(request):
 
 def delivery(request):
 
+    # Categories to show.
+    boy_toys       = Category.objects.filter(parent__name="Игрушки для мальчиков")
+    stuffed_toys   = Category.objects.filter(parent__name="Мягкие игрушки")
+    girl_toys      = Category.objects.filter(parent__name="Игрушки для девочек")
+    new_toys       = Category.objects.filter(name="Новинки")
+    children_books = Category.objects.filter(name="Детские книги")
+    hellium_balls  = Category.objects.filter(name="Гелиевые шары")
     text = InfoPage.objects.filter(name="условия доставки")
 
     return render(request, "blank.html", locals())
 
 
 def adress(request):
+
+    # Categories to show.
+    boy_toys       = Category.objects.filter(parent__name="Игрушки для мальчиков")
+    stuffed_toys   = Category.objects.filter(parent__name="Мягкие игрушки")
+    girl_toys      = Category.objects.filter(parent__name="Игрушки для девочек")
+    new_toys       = Category.objects.filter(name="Новинки")
+    children_books = Category.objects.filter(name="Детские книги")
+    hellium_balls  = Category.objects.filter(name="Гелиевые шары")
 
     text = InfoPage.objects.filter(name="адрес")
 
@@ -25,6 +49,14 @@ def adress(request):
 
 
 def refund(request):
+
+    # Categories to show.
+    boy_toys       = Category.objects.filter(parent__name="Игрушки для мальчиков")
+    stuffed_toys   = Category.objects.filter(parent__name="Мягкие игрушки")
+    girl_toys      = Category.objects.filter(parent__name="Игрушки для девочек")
+    new_toys       = Category.objects.filter(name="Новинки")
+    children_books = Category.objects.filter(name="Детские книги")
+    hellium_balls  = Category.objects.filter(name="Гелиевые шары")
 
     text = InfoPage.objects.filter(name="возврат")
 
