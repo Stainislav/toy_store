0e42ead49f6ce144e6ed5463b4424273513ef95b
Stainislav <swatogor@mail.ru>
2018-09-23 20:24:23 +0300

    Added urls for info pages

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,6 @@ from orders import views
 urlpatterns = [
     url(r'^basket_adding/$', views.basket_adding, name='basket_adding'),    
     url(r'^checkout/$', views.checkout, name='checkout'),
+    url(r'^cart/$', views.cart, name='cart'),
 ]
 
