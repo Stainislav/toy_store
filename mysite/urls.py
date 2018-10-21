a047005854f7dde0ee13ae15921a6c5e3b92b22f
Stainislav <swatogor@mail.ru>
2018-10-21 11:04:50 +0300

    Added some custom names to admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from products import views
 
+admin.site.site_header = "Административная панель"
+admin.site.site_title  = "Административная панель"
+admin.site.index_title = "Добро пожаловать в административную панель!"
+
 urlpatterns = [
     url(r'^$',         views.home, name="home"),
     url(r'^admin/',    admin.site.urls),
